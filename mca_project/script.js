49a897035bde03d96369013864427e65fc7e1427
Sample-max <sstgoodalways546@gmail.com>
2025-03-24 00:21:53 +0530

    Add files via upload

---
+++ mca_project/script.js
@@ -0,0 +1,16 @@
+document.getElementById("btn1").onclick = function () {
+    window.location.href = "Signup.html"; // Load About.html inside iframe
+  };
+  document.getElementById("btn2").onclick = function () {
+    window.location.href = "Login.html"; // Load About.html inside iframe
+  };
+document.getElementById("btn3").onclick = function () {
+  window.location.href = "About.html"; // Load About.html inside iframe
+};
+document.getElementById("btn4").onclick = function () {
+    window.location.href = "Portfolio.html"; // Load About.html inside iframe
+  };
+  document.getElementById("btn5").onclick = function () {
+    window.location.href = "Contact.html"; // Load About.html inside iframe
+  };
+  
